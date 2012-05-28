c4178f3618e9915c60ab85abe8076eb071535542
Lukasz Szpakowski <luckboy@vp.pl>
2012-05-29 01:49:51 +0200

    Added some type synonyms.

--- src/main/org/lbkgraph/base/package.scala
+++ src/main/org/lbkgraph/base/package.scala
@@ -3,4 +3,8 @@ package org.lbkgraph
 package object base
 {
   type GraphBound[V, X, E[+Y, +Z] <: EdgeLike[Y, Z, E] , +G <: GraphLike[V, X, E, G] with Graph[V, X, E]] = GraphLike[V, X, E, G] with Graph[V, X, E]
+
+  type UnwGraphBound[V, W, E[+Y, +Z] <: EdgeLike[Y, Z, E] , +G <: GraphLike[V, Unweighted, E, G] with Graph[V, Unweighted, E]] = GraphLike[V, Unweighted, E, G] with Graph[V, Unweighted, E]
+  
+  type WGraphBound[V, W, E[+Y, +Z] <: EdgeLike[Y, Z, E] , +G <: GraphLike[V, Weighted[W], E, G] with Graph[V, Weighted[W], E]] = GraphLike[V, Weighted[W], E, G] with Graph[V, Weighted[W], E]
 }
