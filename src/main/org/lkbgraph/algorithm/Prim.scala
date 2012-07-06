1ebb2794e47d8c586768d61a4386fa7fc33810f6
Lukasz Szpakowski <luckboy@vp.pl>
2012-07-06 19:12:10 +0200

    Implemented Prim's algorithm (untested).

--- src/main/org/lkbgraph/algorithm/Prim.scala
+++ src/main/org/lkbgraph/algorithm/Prim.scala
@@ -25,12 +25,57 @@ import org.lkbgraph.immutable._
  */
 class Prim[V, W, G <: base.GraphBound[V, Weighted[W], UndiEdge, G]](g: G)(implicit cmp: Ordering[W])
 {
+  import cmp._
+  
   /** Finds the minimum spanning tree by Prim's algorithm. */
   def primMinSpanningTree: Option[G] =
-    Prim.minSpanningTree[V, W, G](g)
+    if(!g.isEmpty) {
+      val h = primMinSpanningTreeFrom(g.vertices.head)
+      if(h.vertices.size == g.vertices.size) Some(h) else None
+    } else
+      None
 
   /** Finds the minimum spanning trees for the connected components by Prim's algorithm. */
-  def primMinSpanningTrees: Set[G] = throw new Exception
+  def primMinSpanningTrees: Set[G] = {
+    var vs = collection.mutable.Set[V]() ++ g.vertices
+    var hs = collection.mutable.Set[G]()
+    while(!vs.isEmpty) {
+      val h = primMinSpanningTreeFrom(vs.head)
+      vs --= h.vertices
+      hs += h
+    }
+    hs.toSet
+  }
+  
+  /** Finds the minimum spanning tree by Prim's algorithm from the specified vertex. */
+  def primMinSpanningTreeFrom(s: V): G = {
+    val b = g.newGraphBuilder
+    if(g.containsVertex(s)) {
+      val n = g.vertices.size
+      val q = collection.mutable.PriorityQueue[UndiEdge[V, Weighted[W]]]()(new Ordering[UndiEdge[V, Weighted[W]]] {
+        override def compare(e1: UndiEdge[V, Weighted[W]], e2: UndiEdge[V, Weighted[W]]): Int =
+          -cmp.compare(e1.weight, e2.weight)
+      })
+      val vs = collection.mutable.Set[V](s)
+      b += V(s)
+      for(e <- g.edgesFrom(s)) {
+        q.enqueue(e)
+        vs += e.out
+        b += e
+      }
+      while(!q.isEmpty) {
+        val v = q.dequeue().out 	// the vertex that has minimum weight
+        val es = g.edgesFrom(v).filterNot { e => vs.contains(e.out) }
+        if(!es.isEmpty) {
+          val minE = es.minBy { _.weight }
+          q.enqueue(minE)
+          vs += minE.out
+          b += minE
+        }
+      }
+    }
+    b.result
+  }
 }
 
 /** A singleton for the implementation of Prim's algorithm.
@@ -42,6 +87,9 @@ object Prim extends MinSpanningTreeStrategy
   implicit def graphToPrim[V, W, G <: base.GraphBound[V, Weighted[W], UndiEdge, G]](g: base.GraphBound[V, Weighted[W], UndiEdge, G])(implicit cmp: Ordering[W]) =
     new Prim[V, W, base.GraphBound[V, Weighted[W], UndiEdge, G]](g)
 
+  override def minSpanningTree[V, W, G <: base.GraphBound[V, Weighted[W], UndiEdge, G]](g: G)(implicit cmp: Ordering[W]): Option[G] =
+    new Prim[V, W, G](g).primMinSpanningTree
+    
   override def minSpanningTrees[V, W, G <: base.GraphBound[V, Weighted[W], UndiEdge, G]](g: G)(implicit cmp: Ordering[W]): Set[G] =
     new Prim[V, W, G](g).primMinSpanningTrees
 }
