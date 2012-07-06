1c79955916c9f46ccede884e6e557832c9507d96
Lukasz Szpakowski <luckboy@vp.pl>
2012-07-06 14:41:59 +0200

    Implemented Kruskal's algorithm (untested).

--- src/main/org/lkbgraph/algorithm/Kruskal.scala
+++ src/main/org/lkbgraph/algorithm/Kruskal.scala
@@ -18,6 +18,7 @@
 package org.lkbgraph.algorithm
 import org.lkbgraph._
 import org.lkbgraph.immutable._
+import org.lkbgraph.mutable.DisjointSet
 
 /** A class for the implementation of Kruskal's algorithm.
  * 
@@ -25,12 +26,34 @@ import org.lkbgraph.immutable._
  */
 class Kruskal[V, W, G <: base.GraphBound[V, Weighted[W], UndiEdge, G]](g: G)(implicit cmp: Ordering[W])
 {
+  import cmp._
+  
   /** Finds the minimum spanning tree by Kruskal's algorithm. */
   def kruskalMinSpanningTree: Option[G] = 
     Kruskal.minSpanningTree[V, W, G](g)
 
   /** Finds the minimum spanning trees for the connected components by Kruskal's algorithm. */
-  def kruskalMinSpanningTrees: Set[G] = throw new Exception
+  def kruskalMinSpanningTrees: Set[G] = {
+    val es = g.edges.toSeq.sortWith { (e1, e2) => e1.weight < e1.weight }
+    val dsns = g.vertices.map { v => (v, DisjointSet.Node(())) }.toMap
+    val fls = collection.mutable.Map() ++ g.vertices.map { 
+      (_, collection.mutable.DoubleLinkedList[UndiEdge[V, Weighted[W]]]())
+    }
+    for(e <- es) {
+      val ds1 = dsns(e._1).toDisjointSet
+      val ds2 = dsns(e._2).toDisjointSet
+      // Whether does an edge connect two unconnected trees?
+      if(ds1 != ds2) {
+        // An edge doesn't connect two unconnected trees, and then there connect two unconnected trees.
+        ds1 |= ds2
+        val fs = fls(e._1)
+        fs.insert(fls(e._2))
+        fls(e._1) = fs
+        fls(e._2) = fs
+      }
+    }
+    fls.values.map { fs => (g.newGraphBuilder ++= fs).result }.toSet
+  }
 }
 
 /** A singleton for the implementation of Kruskal's algorithm.
